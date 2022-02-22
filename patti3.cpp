e57d35cbfe23480c6ed3638b4140e4e44e2132c0
pattiiiii <100131924+pattiiiii@users.noreply.github.com>
2022-02-22 19:02:33 +0800

    Create patti3.cpp

---
+++ patti3.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+   cout << "Hello selena";
+   cout <<  "my height is " << 1.51 << "m " << 151 << "cm";
+    return 0;
+}
